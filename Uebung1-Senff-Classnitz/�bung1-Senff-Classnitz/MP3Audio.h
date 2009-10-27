a8a39f034303f4d9bff93895acda7c9b562b46e9
Daniel Senff <mail@danielsenff.de>
2009-10-27 10:39:31 -0800

    added id3lib library, sortof

--- Uebung1-Senff-Classnitz/�bung1-Senff-Classnitz/MP3Audio.h
+++ Uebung1-Senff-Classnitz/�bung1-Senff-Classnitz/MP3Audio.h
@@ -3,7 +3,13 @@
 class MP3Audio
 {
 private:
+  const char* file_name;
+  const char* title;
+  const char* interpret;
+  const char* album;
+  const char* genre;
 
+  
 
 public:
   MP3Audio(void);
