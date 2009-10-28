58a3e1448dc1549eb69f742fb585262caf81c000
Daniel Senff <mail@danielsenff.de>
2009-10-28 12:57:32 -0800

    added git ignore to leave out build data

--- Task1/Task1/Form1.h
+++ Task1/Task1/Form1.h
@@ -97,7 +97,31 @@ namespace Task1 {
        this->OpenFileDialog1->Multiselect = false;
        this->OpenFileDialog1->Title = "My MP3 Browser";
     }
-
+    // This method handles the FileOK event.  It opens each file 
+    // selected and loads the image from a stream into PictureBox1.
+    void OpenFileDialog1_FileOk( Object^ sender,
+       System::ComponentModel::CancelEventArgs^ e )
+    {
+       this->Activate();
+       array<String^>^ files = OpenFileDialog1->FileNames;
+       
+       // Open each file and display the image in PictureBox1.
+       // Call Application.DoEvents to force a repaint after each
+       // file is read.        
+       for each ( String^ file in files )
+       {
+          System::IO::FileInfo^ fileInfo = gcnew System::IO::FileInfo( file );
+          System::IO::FileStream^ fileStream = fileInfo->OpenRead();
+          PictureBox1->Image = System::Drawing::Image::FromStream( fileStream );
+          Application::DoEvents();
+          fileStream->Close();
+          
+          // Call Sleep so the picture is briefly displayed, 
+          //which will create a slide-show effect.
+          System::Threading::Thread::Sleep( 2000 );
+       }
+       PictureBox1->Image = nullptr;
+    }
 
   private: System::Void btOpen_Click(System::Object^  sender, System::EventArgs^  e) {
     OpenFileDialog1->ShowDialog();
