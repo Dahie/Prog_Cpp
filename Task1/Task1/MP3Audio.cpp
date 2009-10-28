f0c74be93cfe3aa815db6517339b7c6fa56685ff
Daniel Senff <mail@danielsenff.de>
2009-10-28 12:11:02 -0800

    first pass on the mp3audio class, supports reading title information, basis for more

--- Task1/Task1/MP3Audio.cpp
+++ Task1/Task1/MP3Audio.cpp
@@ -1,6 +1,25 @@
 #include "StdAfx.h"
 #include "MP3Audio.h"
+#include <id3/tag.h>
 
-MP3Audio::MP3Audio(void)
-{
+
+MP3Audio* MP3Audio::read(const char* file){
+
+  ID3_Tag tag(file);
+
+  const char* ptitle;
+  ID3_Frame* frame_title = tag.Find(ID3FID_TITLE);
+  if(frame_title != 0) {
+   ptitle = frame_title->GetField(ID3FN_TEXT)->GetRawText();
+  }
+
+ const char* palbum;
+  const char* pfilename;
+  const char* pinterpret; 
+  const char* pgenre;
+
+  MP3Audio mp3(pfilename, ptitle, pinterpret, palbum, pgenre);
+
+  return &mp3;
+
 }
