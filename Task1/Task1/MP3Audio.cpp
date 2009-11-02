033ac927d187934565de90427c0ba718bf06ebde
Daniel Senff <mail@danielsenff.de>
2009-11-02 11:40:34 -0800

    mp3audio class constructor and opendialog filename convertion

--- Task1/Task1/MP3Audio.cpp
+++ Task1/Task1/MP3Audio.cpp
@@ -3,20 +3,22 @@
 #include <id3/tag.h>
 
 
-MP3Audio* MP3Audio::read(const char* file){
+MP3Audio* MP3Audio::read(std::string& file){
 
-  ID3_Tag tag(file);
+  // TODO check if it's an mp3
+  
+  ID3_Tag tag(file.c_str());
 
-  const char* ptitle;
+  const char* ptitle = 0;
   ID3_Frame* frame_title = tag.Find(ID3FID_TITLE);
   if(frame_title != 0) {
-   ptitle = frame_title->GetField(ID3FN_TEXT)->GetRawText();
+    ptitle = frame_title->GetField(ID3FN_TEXT)->GetRawText();
   }
 
- const char* palbum;
-  const char* pfilename;
-  const char* pinterpret; 
-  const char* pgenre;
+  const char* palbum = 0;
+  const char* pfilename = 0;
+  const char* pinterpret = 0; 
+  const char* pgenre = 0;
 
   MP3Audio mp3(pfilename, ptitle, pinterpret, palbum, pgenre);
 
