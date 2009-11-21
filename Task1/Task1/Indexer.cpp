8f404d3ef6c94f286d0291e1874666e9ee7d6404
Daniel Senff <mail@danielsenff.de>
2009-11-21 10:01:31 -0800

    added stubs for implementations of find and keypair

--- Task1/Task1/Indexer.cpp
+++ Task1/Task1/Indexer.cpp
@@ -12,23 +12,46 @@ Indexer::~Indexer(void)
 void Indexer::add(std:string& word, MP3:MP3Audio* mp3audio) 
 {
   // find if the word already exists
+  // logarithmic/binary search
 
   // add mp3audio to the words' associated list
 
 }
+
+KeyPair* Indexer::find(std:string& key) const
+{
+  KeyPair* keypair
+
+  // logarithmix search
+  int i = (int)num_words*0.5;
+  while(current_keypair->key != key) 
+  {
+    
+    current_keypair = list[i];
+    
+  }
+
+  // return the found keypair
+  return keypair*
+}
+
 MP3:MP3Audio* Indexer::findFirst(std:string& key) const
 {
-  // find word
+ 
 
-  // return the first element in the associated list
+  
 
 }
 
 CSortedTracks* Indexer::findAll(std:string& key) const
 {
-  // find word
+   // find word
+  KeyPair* keypair = find(key);
+  if(keypair != 0) {
+  
+    // return the first element in the associated list
+    keypair->set
 
-  // get list of associated elements
-
-  // return list in SortedTrack-list
+  } else 
+    return 0;
 }
