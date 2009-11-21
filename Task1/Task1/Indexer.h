304a61a9f76fe70de3e81b04b6df50b5fae531e7
Daniel Senff <mail@danielsenff.de>
2009-11-21 08:34:08 -0800

    introducing keypair for indexing class

--- Task1/Task1/Indexer.h
+++ Task1/Task1/Indexer.h
@@ -5,13 +5,35 @@
 #include "SortedTracks.h"
 
 
+class KeyPair
+{
+private:
+  // key
+  std:string& key;
+  // associated list of elements
+  // TODO
+public:
+  KeyPair(void);
+  KeyPair(std:string& pkey): _key(pkey);
+  ~KeyPair(void);
+
+}
+
 class Indexer
 {
+protected:
+  int num_words;
+  
+  
+  KeyPair[] list;
+
+
 public:
   Indexer(void);
   ~Indexer(void);
 
   void add(std:string& word, MP3:MP3Audio* mp3audio);
+  KeyPair* find(std:string& key) const;
   MP3:MP3Audio* findFirst(std:string& key) const;
   CSortedTracks* findAll(std:string& key) const;
 
