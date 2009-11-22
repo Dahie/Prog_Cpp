c1225f75f1e7d22ab5340a6e45b5b794186154bb
Daniel Senff <mail@danielsenff.de>
2009-11-22 07:23:23 -0800

    work on keypair struct and implementing the insert and search

--- Task1/Task1/Indexer.cpp
+++ Task1/Task1/Indexer.cpp
@@ -1,20 +1,68 @@
 #include "StdAfx.h"
 #include "Indexer.h"
 
+using namespace Search;
+
+
 Indexer::Indexer(void)
 {
+  capacity = growth;
+  num_words = 0;
+  list = new KeyPair[capacity];
 }
 
 Indexer::~Indexer(void)
 {
+  delete list;
 }
 
 void Indexer::add(std:string& word, MP3:MP3Audio* mp3audio) 
 {
   // find if the word already exists
-  // logarithmic/binary search
+  KeyPair* keypair = find(key);
+  if(keypair != 0) {
+    // word already exists
+    // add mp3audio to the words' associated list
+  } else {
+    // word not yet indexed
 
-  // add mp3audio to the words' associated list
+    // create keypair
+    KeyPair* keypair;
+    keypair->word = word;
+// TODOkeypair->list
+    keypair->addAudio(mp3audio);
+
+    // find insert index one behind the la
+    int insert_index = num_words++;
+
+    // determine if the capacity of the current list is exceeded
+    if(insert_index++ > capacity) {
+      // number of elements exceeded capacity
+      // expand list
+
+      // create new longer list
+      KeyPair* new_list = new KeyPair[capacity+growth];
+
+      // copy all elements
+      for (int i = 0; i < num_words; ++i) {
+        new_list[i] = list[i];
+      }
+
+      // delete old list
+      delete[] list;
+
+      // replace list
+      list = new_list;
+    }
+
+
+    // add keypair to list
+      list[insert_index] = keypair;
+
+    // resort list to alphabetical ASC
+    // TODO later
+
+  }
 
 }
 
@@ -50,7 +98,7 @@ CSortedTracks* Indexer::findAll(std:string& key) const
   if(keypair != 0) {
   
     // return the first element in the associated list
-    keypair->set
+    keypair->set->first
 
   } else 
     return 0;
